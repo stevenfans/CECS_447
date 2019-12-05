2132d75366e090a320749b93668e8d13512eeadc
Rosswell Tiongco <rosswelltiongco@gmail.com>
2019-12-04 17:08:32 -0800

    Added working lcd code

--- Infrared_Receiver/TM4C_2_Receiver/lab2_graphics.c
+++ Infrared_Receiver/TM4C_2_Receiver/lab2_graphics.c
@@ -6,16 +6,6 @@
 // Possible main program to test the lab
 // Feel free to edit this to match your specifications
 
-// Backlight (pin 10) connected to +3.3 V
-// MISO (pin 9) unconnected 
-// SCK (pin 8) connected to PA2 (SSI0Clk)
-// MOSI (pin 7) connected to PA5 (SSI0Tx)
-// TFT_CS (pin 6) connected to PA3 (SSI0Fss)
-// CARD_CS (pin 5) unconnected
-// Data/Command (pin 4) connected to PA6 (GPIO)
-// RESET (pin 3) connected to PA7 (GPIO)
-// VCC (pin 2) connected to +3.3 V
-// Gnd (pin 1) connected to ground
 
 #include <stdio.h>
 #include <stdint.h>
---
+++ Infrared_Receiver/TM4C_2_Receiver/lab2_graphics.d
@@ -0,0 +1,8 @@
+.\lab2_graphics.o: lab2_graphics.c
+.\lab2_graphics.o: C:\Keil\ARM\ARMCC\bin\..\include\stdio.h
+.\lab2_graphics.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\lab2_graphics.o: C:\Keil\ARM\ARMCC\bin\..\include\string.h
+.\lab2_graphics.o: ST7735.h
+.\lab2_graphics.o: PLL.h
+.\lab2_graphics.o: walking.h
+.\lab2_graphics.o: tm4c123gh6pm.h
